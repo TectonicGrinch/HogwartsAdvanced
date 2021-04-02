81737e2d515e91f95d90a94c58d78118c5d27cfd
ViridianZe <40783616+TectonicGrinch@users.noreply.github.com>
2021-04-02 19:40:50 +1100

    Minor changes & Location.js Locate.js Merge

--- Commands/Location.js
+++ Commands/Location.js
@@ -1,34 +1,73 @@
 const Discord = require("discord.js");
+const world = require('../json/world.json');
+const items = require('../json/items/items.json');
 
 module.exports.run = async (bot, message, args) => {
-	if (!args.length) return message.channel.send(bot.embed(bot.config.locations.join(",\n ")).setTitle("Locations"));
-	let city = args.join(" ").toLowerCase();
-	let requiredItems = getRequiredItemsToTravelTo(city, bot.itemIndex.allItems);
-	let inv = bot.db.get(message.author.id).inv;
-	let itemIdx = inv.findIndex((i) => requiredItems.includes(i.name));
 
-	if (itemIdx == -1)
-		return message.channel.send(
-			bot.embed(`You dont have any item that lets you travel there. ( ${requiredItems.join(", ") || "No way to get there"})`));
-	let item = inv[itemIdx];
-	if (!item.infinite) {
-		inv.splice(itemIdx, 1);
-		bot.db.set(`${message.author.id}.inv`, inv);
+    
+	let command = args[0];
+	let city = args[1].toLowerCase();
+	console.log(city)
+	let requiredItems = getRequiredItemsToTravelTo(city, items.allitems);
+	let inv = bot.db.get(message.author.id).inv;
+
+
+	let user = message.author;
+	let locObj = bot.db.get(user.id) || {};
+	if(!command)
+    
+	message.channel.send(bot.embed(`**${user.username}** 
+	Location: **${locObj.location || "undefined"}**`));
+	console.log(`${user} is located at ${locObj.location || "undefined"}`);
+	if(command){
+		switch(command){
+			case 'list':
+				let ret = '';
+				for (i in world) {
+					let location = world[i];
+					ret += `\n${location.name},`;
+
+				}
+
+
+				 message.channel.send(bot.embed(`${ret}`));
+
+				break;
+
+			case 'travel':
+				let itemIdx = locObj.inv.findIndex((i) => requiredItems.includes(i.name));
+				console.log(itemIdx)
+				if (itemIdx == -1)
+					return message.channel.send(
+						bot.embed(`You dont have any item that lets you travel there. ( ${requiredItems.join(", ") || "No way to get there"})`));
+				let item = inv[itemIdx];
+				if (!item.infinite) {
+					inv.splice(itemIdx, 1);
+					bot.db.set(`${message.author.id}.inv`, inv);
+				}
+				bot.db.set(`${message.author.id}.location`, city);
+				 message.channel.send(bot.embed(`You\'ve travelled to **${city}**`));
+				break;
+        }
 	}
-	bot.db.set(`${message.author.id}.location`, city);
-	return message.channel.send(bot.embed(`You\'ve travelled to **${city}**`));
-	//Code End
+
+
+
+
+
+
+
 };
 
-function getRequiredItemsToTravelTo(city, allItems) {
-	let items = [];
-	for (let i in allItems) {
-		if (allItems[i].location == city) {
-			items.push(i);
+function getRequiredItemsToTravelTo(city, items) {
+	let ret = [];
+	for (let i in items) {
+		if (items[i].location == city) {
+			ret.push(i);
 			continue;
 		}
 	}
-	return items;
+	return ret;
 }
 
 module.exports.config = {
