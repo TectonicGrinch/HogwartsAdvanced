9f37270a998ca369225e30b56b963dc8e8e2f994
ViridianZe <40783616+TectonicGrinch@users.noreply.github.com>
2021-04-14 00:03:20 +1000

    functionable changes to testitem and invFuncs

--- Commands/items/testitem.js
+++ Commands/items/testitem.js
@@ -6,8 +6,7 @@ module.exports.run = async (bot, message, args) => {
     if(!item){
         return message.channel.send(`you didn't list an item.`);
     }
-    if(!invFuncs.invHasItem(message.author, item)){
-        return message.channel.send(`You do not have the \n**item:** ${item} \nor it does not exist`)
+    if(!invFuncs.invHasItem(message.author, item, message, bot)){
     }
     if(item) {
         for(i in item){
@@ -17,8 +16,8 @@ module.exports.run = async (bot, message, args) => {
             if(item == 'broomstick')
                 return message.channel.send(`${item} achieved!`);
             
-            if(item == 'test3')
-                return message.channel.send(`test3 achieved!`);
+            if(item == 'test')//if this pops up it means something checking items is broken
+                return message.channel.send(`test passed!`);
                 
             }
         }
--- Util/invFuncs.js
+++ Util/invFuncs.js
@@ -2,14 +2,15 @@ const fs = require('fs');
 const db = require('quick.db')
 const itemIndex = require('../json/items/items')
 
-function invHasItem(user, itemName) {
+function invHasItem(user, itemName, message, bot) {
       let userDB = db.get(user.id)
       let item = itemIndex
       for(o in item){
         if(userDB.inv.some(i => i.name == itemName)){
           return true;
         }else{
-          return false;
+          return message.channel.send(bot.embed(`You do not have the \n**item:** ${itemName} \nor it does not exist, Use inv to see you inventory.`));
+          
         }
       }
     }
